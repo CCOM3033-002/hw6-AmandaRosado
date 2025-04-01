0615989d6ae7396a71168180f62d213c58682087
Amanda <amanda.rosado3@upr.edu>
2025-04-01 01:40:37 +0000

    commit1.4

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ Amanda Rosado Rosado
 Asignacion 6: Figuras en 3D
 
 Proposito:
-Este programa que le pida al usuario que escoja entre las sigueintes figuras en 3 dimensiones:
-cilindro, esfera, prisma rectangular. Luego le pregunte si quiere calcular el volumen
+Este programa le pide al usuario que escoja entre las siguientes figuras en 3 dimensiones:
+cilindro, esfera, prisma rectangular. Luego le pregunta si quiere calcular el volumen
 o el área de la superficie de la figura escogida. 
 */
 
@@ -22,6 +22,14 @@ double area_superficie(double radio2, double altura2);
 
 double volumen(double radio3);
 
+double area_superficie(double radio4);
+
+double volumen(double ancho, double largo, double altura3);
+
+double area_superficie(double longitud, double ancho2, double altura4);
+
+//------------------------------------------------------------------------------------------------------------
+
 
 //Se crea funcion main
 int main () {
@@ -30,15 +38,13 @@ int main () {
     //Se le deja saber el proposito del programa al usuario
     cout << "\nEste programa calcula el volumen y el área de la supercicie de tres figuras." << endl;
 
-    cout << "\nEscoga una figura: "<< endl;
+    cout << "\nEscoge una figura: "<< endl;
     cout << "a. Cilindro" << endl;
     cout << "b. Esfera" << endl;
     cout << "c. Prisma rectangular" << endl;
     cout << "\n";
 
-    char Cilindro = 'a';
-    char Esfera = 'b';
-    char Prisma_Rectangular = 'c';
+
     char respuesta1;
 
     cin >> respuesta1;
@@ -51,13 +57,12 @@ int main () {
     }
 
 
-    cout << "\nEscoga entre los siguientes opciones: " << endl;
+    cout << "\nEscoge entre las siguientes opciones: " << endl;
     cout << "a. Volumen" << endl;
-    cout << "b. Área de la superficia" << endl;
+    cout << "b. Área de la superficie" << endl;
     cout << "\n";
 
-    char Volumen = 'a';
-    char Area_De_Superficie = 'b';
+
     char respuesta2;
 
     cin >> respuesta2;
@@ -69,6 +74,7 @@ int main () {
         cin >> respuesta2;
     }
 
+//-------------------------------------------------------------------------------------------------------------
 
     //Si las respuestas del usuario son 'a' y 'a' se hace este if
     if (respuesta1 == 'a' && respuesta2 == 'a') {
@@ -110,14 +116,66 @@ int main () {
         cout << "\nEntre el radio de su esfera: " << endl;
         cin >> radio3;
     
+        //Se invoca funcion volumen para su esfera
         cout << "\nEl volumen de su esfera es: " << volumen(radio3) << endl;
     }
 
 
+    //Si las respuestas del usuario son 'b' y 'b' se hace este if
+    if (respuesta1 == 'b' && respuesta2 == 'b') {
 
+        double radio4;
+
+        cout << "\nEntre el radio de su esfera: " << endl;
+        cin >> radio4;
+
+        //Se invoca funcion area_superficie para su esfera
+        cout << "\nEl area de superficie de su esfera es: " << area_superficie(radio4) << endl;
+    }
+
+
+    //Si las respuestas del usuario son 'c' y 'a' se hace este if
+    if (respuesta1 == 'c' && respuesta2 == 'a') {
+
+        double ancho, largo, altura3;
+
+        cout << "\nEntre el ancho de su prisma rectangular: " << endl;
+        cin >> ancho;
+
+        cout << "\nEntre el largo de su prisma rectangular: " << endl;
+        cin >> largo;
+
+        cout << "\nEntre la altura de su prisma rectangular: " << endl;
+        cin >> altura3;
+
+        //Se invoca funcion volumen para su prisma rectangular
+        cout << "\nEl volumen de su prisma rectangular es: " << volumen(ancho, largo, altura3) << endl;
+    }
+
+
+    //Si las respuestas del usuario son 'c' y 'b' se hace este if
+    if (respuesta1 == 'c' && respuesta2 == 'b') {
+
+        double longitud, ancho2, altura4;
+
+        cout << "\nEntre la longitud de su prisma rectangular: " << endl;
+        cin >> longitud;
+
+        cout << "\nEntre el ancho de su prisma rectangular: " << endl;
+        cin >> ancho2;
+
+        cout << "\nEntre la altura de su prisma rectangular: " << endl;
+        cin >> altura4;
+
+        //Se invoca funcion area_superficie para su prisma rectangular
+        cout << "\nEl area de superficie de su prisma rectangular es: " << area_superficie(longitud, ancho2, altura4) << endl;
+    } 
 
     return 0;
 }
+
+
+//-------------------------------------------------------------------------------------------------------------
 
 
 //Funcion para calcular el volumen de un cilindro
@@ -138,4 +196,25 @@ double area_superficie(double radio2, double altura2) {
 double volumen(double radio3) {
 
     return ((4.0/3.0) * M_PI * pow(radio3, 3));
+}
+
+
+//Funcion para calcular el area de superficie de una esfera
+double area_superficie(double radio4) {
+
+    return (4 * M_PI * radio4 * 2);
+}
+
+
+//Funcion para calcular el volumen de una prisma rectangular
+double volumen(double ancho, double largo, double altura3) {
+
+    return (ancho * largo * altura3);
+}
+
+
+//Funcion para calcular el area de superficie de una prisma rectangular
+double area_superficie(double longitud, double ancho2, double altura4) {
+
+    return 2 * (longitud * ancho2 + longitud * altura4 + altura4 * ancho2);
 }
